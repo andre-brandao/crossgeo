39b90f96a284ecade583ac36a5bcefff5c7a2685
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-08-20 16:40:18 -0300

    migration 1

---
+++ migrations/0000_damp_alex_wilder.sql
@@ -0,0 +1,156 @@
+CREATE TABLE `magic_link` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`email` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `password_reset_code` (
+	`token_hash` text NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`id` text PRIMARY KEY NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`username` text NOT NULL,
+	`email` text NOT NULL,
+	`email_verified` integer DEFAULT false NOT NULL,
+	`password_hash` text NOT NULL,
+	`permissions` text DEFAULT '{"role":"user"}' NOT NULL,
+	`used_credits` integer DEFAULT 0,
+	`max_credits` integer DEFAULT 200
+);
+--> statement-breakpoint
+CREATE TABLE `user_verification_code` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`code` text NOT NULL,
+	`user_id` text NOT NULL,
+	`email` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `image` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`uploaded_by` text,
+	`name` text NOT NULL,
+	`data` blob NOT NULL,
+	FOREIGN KEY (`uploaded_by`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE set null
+);
+--> statement-breakpoint
+CREATE TABLE `bugReport` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`created_by` text DEFAULT '',
+	`status` text NOT NULL,
+	`name` text NOT NULL,
+	`metadata` text,
+	FOREIGN KEY (`created_by`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE set null
+);
+--> statement-breakpoint
+CREATE TABLE `logs` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`created_by` text DEFAULT '',
+	`text` text NOT NULL,
+	`metadata` text,
+	`error` text,
+	FOREIGN KEY (`created_by`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE set null
+);
+--> statement-breakpoint
+CREATE TABLE `notification_channel` (
+	`channel_id` text PRIMARY KEY NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP)
+);
+--> statement-breakpoint
+CREATE TABLE `notification_channel_users` (
+	`channel_id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `push_notification_device` (
+	`device_id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` text NOT NULL,
+	`subscription` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `push_notification_log` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`device_id` integer,
+	`channel_id` text,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`payload` text NOT NULL,
+	`http_status` integer NOT NULL,
+	`success` integer NOT NULL,
+	`err_message` text,
+	FOREIGN KEY (`device_id`) REFERENCES `push_notification_device`(`device_id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`channel_id`) REFERENCES `notification_channel`(`channel_id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `chart` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`data_id` integer NOT NULL,
+	`type` text NOT NULL,
+	`title` text NOT NULL,
+	`filters` text NOT NULL,
+	FOREIGN KEY (`data_id`) REFERENCES `map_data`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `map_data` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`map_id` integer NOT NULL,
+	`name` text,
+	`fields` text NOT NULL,
+	FOREIGN KEY (`map_id`) REFERENCES `map`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `point` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`data_id` integer NOT NULL,
+	`latitude` real NOT NULL,
+	`longitude` real NOT NULL,
+	`meta` text,
+	FOREIGN KEY (`data_id`) REFERENCES `map_data`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `map` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
+	`name` text NOT NULL,
+	`created_by` text NOT NULL,
+	`latitude` real,
+	`longitude` real,
+	FOREIGN KEY (`created_by`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `stripe_checkout_session` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`geopoints` integer NOT NULL,
+	`stripe_json` text NOT NULL,
+	`credited` integer DEFAULT false NOT NULL,
+	`expires_at` integer NOT NULL,
+	`expired` integer DEFAULT false NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE set null
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `user_username_unique` ON `user` (`username`);--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint
+CREATE UNIQUE INDEX `notification_channel_channel_id_unique` ON `notification_channel` (`channel_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `push_notification_device_subscription_unique` ON `push_notification_device` (`subscription`);
