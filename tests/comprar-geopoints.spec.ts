c6d028e01ac22f886fb24ff9cd4164e93f6cba63
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-15 16:20:20 -0300

    Adding playwright test that check if it is possible to buy GeoPoints

---
+++ tests/comprar-geopoints.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test'
+import { user as userController } from '../src/lib/server/db/schema/user/controller'
+import { deleteUserForTesting } from './utils'
+import { User } from './user'
+
+// Criando usuário de teste
+const testUser = new User(
+  'john.doe',
+  'john.doe@email.example',
+  true,
+  '+55 (99) 9 9999-9999',
+  true,
+  'ex1@!ple',
+  { role: 'user' },
+  0,
+  100,
+)
+
+/**
+ * Função a ser executada antes de iniciar os testes.
+ */
+test.beforeAll(async () => {
+  // Adicionando usuário de teste ao banco de dados
+  await userController.insertUser(await testUser.toInsertUser())
+})
+
+/**
+ * Função a ser executada após terminar os testes
+ */
+test.afterAll(async () => {
+  // Apagando o usuário criado
+  await deleteUserForTesting(testUser.username)
+})
+
+test('Testando se a compra de GeoPoints leva para o site da Stripe', async ({
+  page,
+}) => {
+  await page.goto('http://localhost:5173/login/password')
+  await page.getByPlaceholder('Email').fill(testUser.email)
+  await page.locator('#password').fill(testUser.password)
+  await page.getByRole('button', { name: 'Continuar' }).click()
+  await page.goto('http://localhost:5173/checkout')
+  await page.getByRole('button', { name: '+1000' }).click()
+  await page.getByRole('button', { name: 'Checkout' }).click()
+  expect(page.url().startsWith('https://checkout.stripe.com')).toBe(true)
+})
